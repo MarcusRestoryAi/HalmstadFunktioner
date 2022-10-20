8700ffb846249bd22fb7d9975e940934591c2ea5
Marcus <marcus.henriksson@restory.ai>
2022-10-20 10:55:09 +0200

    Skapat funktionen MyFunction

--- Funktioner/Program.cs
+++ Funktioner/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //Anropa MyFunction
+            MyFunction();
+            MyFunction();
         }
+
+        //Skapa en ny function som heter myFunction
+        static void MyFunction()
+        {
+            Console.WriteLine("Inuti en Funktion");
+        }
+
+
     }
 }

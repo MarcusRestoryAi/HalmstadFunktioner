937b7842cff2ee16d92bbb1e9d997c6545739ff7
Marcus <marcus.henriksson@restory.ai>
2022-10-20 13:27:46 +0200

    Added Global Variabel CarColor

--- Funktioner/Program.cs
+++ Funktioner/Program.cs
@@ -2,6 +2,9 @@
 {
     internal class Program
     {
+        // Deklarerar en global variabel
+        static string carColor;
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
@@ -11,8 +14,22 @@
             MyFunction();
 
             //Anropa OneOrTwo med ett parameter värde
+            int x = 2;
             OneOrTwo(1);
             OneOrTwo(2);
+            OneOrTwo(x);
+            OneOrTwo(3);
+
+            //Anropar funktionen SayHey och sparar returvärdet i variabel
+            string meddelande = SayHey("Marcus");
+            Console.WriteLine(meddelande);
+
+            Console.WriteLine( SayHey("Anders") );
+
+            //Global vairabel carColor
+            Console.WriteLine("Min bil har färgen {0}", carColor);
+            Car();
+            Console.WriteLine("Min bil har färgen {0}", carColor);
         }
 
         //Skapa en ny function som heter myFunction
@@ -31,6 +48,18 @@
             {
                 Console.WriteLine("Input är 2");
             }
+        }
+
+        //Skapa en funktion som returnerar ett hälsningsmeddelande
+        static string SayHey(string name)
+        {
+            string message = "Hejsan " + name;
+            return message;
+        }
+
+        static void Car()
+        {
+            carColor = "Röd";
         }
     }
 }
